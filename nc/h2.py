26fabf5c8fa712cfa352710943c92224aebc4e68
mpaolodr <mpaolodr_1504@yahoo.com>
2020-10-27 23:02:54 -0400

    median

--- nc/h2.py
+++ nc/h2.py
@@ -7,3 +7,32 @@ def removeEvens(numbers):
             del numbers[i]
 
     return numbers
+
+
+def arrayMedian(sequence):
+
+    # sort in place - mutating the input
+    sequence.sort()
+
+    if len(sequence) % 2 == 0:
+
+        return even_median(sequence)
+
+    else:
+
+        return odd_median(sequence)
+
+
+def odd_median(arr):
+
+    mid = len(arr) // 2
+
+    return arr[mid]
+
+
+def even_median(arr):
+
+    mid1 = len(arr) // 2
+    mid2 = mid1 - 1
+
+    return (arr[mid1] + arr[mid2]) / 2
