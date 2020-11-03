e099720b5e22d51b57e3e981d9b9b1c2d3787d9e
mpaolodr <mpaolodr_1504@yahoo.com>
2020-11-02 21:15:33 -0500

    gp3 prob 1

---
+++ nc/gp/gp3.py
@@ -0,0 +1,54 @@
+"""
+Given an array of integers `nums` and an integer `target`, return the indices
+of the two numbers that add up to the `target`.
+
+Examples:
+
+- two_sum(nums = [2,5,9,13], target = 7) -> [0,1] (nums[0] + nums[1] == 7)
+- two_sum(nums = [4,3,5], target = 8) -> [1,2] (nums[1] + nums[2] == 8)
+
+Notes:
+
+- Each input will have only one solution.
+- You may not use the same element twice.
+- You can return the answer in any order.
+"""
+
+
+def two_sum(nums, target):
+    # Your code here
+
+    elem_indices = dict()
+
+    for i in range(len(nums)):
+
+        if nums[i] not in elem_indices:
+
+            elem_indices[nums[i]] = list()
+
+        elem_indices[nums[i]].append(i)
+
+    for key in elem_indices:
+
+        needed = target - key
+
+        if needed in nums:
+
+            # in case there are duplicates
+            if needed != key:
+
+                return [elem_indices[needed][0], elem_indices[key][0]]
+
+            else:
+
+                # if there are duplicates, we return the first 2,
+                # if key is the ssame as needed and it only has length of 1, we do nothing
+                if len(elem_indices[needed]) > 1:
+
+                    return [elem_indices[needed][0], elem_indices[needed][1]]
+
+    return elem_indices
+
+
+print(two_sum([2, 5, 9, 13], 7))
+print(two_sum([4, 3, 5], 8))
