bee1cf4929956987de01b5071ff00975bc6157b0
mpaolodr <mpaolodr_1504@yahoo.com>
2020-11-04 14:13:24 -0500

    findAdded letter different approach

--- nc/timespacecomp.py
+++ nc/timespacecomp.py
@@ -25,32 +25,62 @@ def csFirstUniqueChar(input_str):
         return -1
 
 
+# def csFindAddedLetter(str_1, str_2):
+
+#     dict_str_1 = dict()
+#     dict_str_2 = dict()
+
+#     for i in range(len(str_1)):
+
+#         if str_1[i] not in dict_str_1:
+
+#             dict_str_1[str_1[i]] = 0
+
+#         dict_str_1[str_1[i]] += 1
+
+#     for i in range(len(str_2)):
+
+#         if str_2[i] not in dict_str_2:
+
+#             dict_str_2[str_2[i]] = 0
+
+#         dict_str_2[str_2[i]] += 1
+
+#     for k in dict_str_2:
+
+#         if k not in dict_str_1 or dict_str_2[k] != dict_str_1[k]:
+
+#             return k
+
+
 def csFindAddedLetter(str_1, str_2):
+    m = len(str_1)
+    n = len(str_2)
 
-    dict_str_1 = dict()
-    dict_str_2 = dict()
+    j = 0    # Index of str1
+    i = 0    # Index of str2
 
-    for i in range(len(str_1)):
+    while j < m and i < n:
 
-        if str_1[i] not in dict_str_1:
+        # if there's a mismatch, that's the added string so we return that
+        if str_1[j] != str_2[i]:
 
-            dict_str_1[str_1[i]] = 0
+            return str_2[i]
 
-        dict_str_1[str_1[i]] += 1
+        # no mismatch, we just increment both pointers (j and i)
+        i += 1
+        j += 1
 
-    for i in range(len(str_2)):
+    # exits the while loop
 
-        if str_2[i] not in dict_str_2:
+    return str_2[i]
 
-            dict_str_2[str_2[i]] = 0
 
-        dict_str_2[str_2[i]] += 1
-
-    for k in dict_str_2:
-
-        if k not in dict_str_1 or dict_str_2[k] != dict_str_1[k]:
-
-            return k
+print(csFindAddedLetter("bcde", "bcdef"))
+print(csFindAddedLetter("", "z"))
+print(csFindAddedLetter("b", "bb"))
+print(csFindAddedLetter("f", "bfb"))
+print(csFindAddedLetter("xqmxtheyvpdqounqmfyaqdqxwe", "xqmxtheyvpdqounqmfyaqxdqxwe"))
 
 
 def csSortedTwoSum(numbers, target):
