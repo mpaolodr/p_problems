aa3777bc8e348ab632f4491a57a67aaa48c17e71
mpaolodr <mpaolodr_1504@yahoo.com>
2020-11-10 22:46:26 -0500

    hashtable i prob 2

--- nc/s4/hw1.py
+++ nc/s4/hw1.py
@@ -16,3 +16,36 @@ def csFindTheSingleNumber(nums):
         if count_dict[k] == 1:
 
             return k
+
+
+def csAverageOfTopFive(scores):
+
+    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+    score_dict = dict()
+
+    for s in sorted_scores:
+
+        if s[0] not in score_dict:
+
+            score_dict[s[0]] = list()
+
+        score_dict[s[0]].append(s[1])
+
+    average_dict = dict()
+
+    for k in score_dict:
+
+        average = sum(score_dict[k][0:5])
+
+        if len(score_dict[k]) >= 5:
+
+            average_dict[k] = [k, average // 5]
+
+        else:
+
+            average_dict[k] = [k, average // len(score_dict[k])]
+
+    sorted_average = [average_dict[key] for key in sorted(average_dict)]
+
+    return sorted_average
