902910465bbe37d1e0115da9c284c0c0082833ee
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-01 23:32:43 -0500

    demo 1

--- nc/gp/gp_bst.py
+++ nc/gp/gp_bst.py
@@ -129,3 +129,109 @@ b3.right.right = TreeNode(15)
 print(b1.is_valid_BST(b1))
 print(b2.is_valid_BST(b2))
 print(b3.is_valid_BST(b3))
+
+
+# demo 1
+"""
+You are given a binary tree.
+Write a function that can find the **maximum depth** of the binary tree. The
+maximum depth can be defined as the number of nodes found along the longest
+path from the root down to the furthest leaf node. Remember, a leaf node is a
+node that has no children.
+Example:
+Given the following binary tree
+    5
+   / \
+  12  32
+     /  \
+    8    4
+your function should return the depth = 3.
+"""
+
+
+class BinaryTreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def insert(self, value):
+
+        if value < self.value:
+
+            if self.left is None:
+
+                self.left = BinaryTreeNode(value)
+
+            else:
+
+                self.left.insert(value)
+
+        else:
+
+            if self.right is None:
+
+                self.right = BinaryTreeNode(value)
+
+            else:
+
+                self.right.insert(value)
+
+    def maxDepth(self, root):
+        # Your code here
+
+        # base case of empty tree
+        # if root is None:
+
+        #     return 0
+
+        # # get left height
+        # left_height = self.maxDepth(root.left)
+
+        # # get right height
+        # right_height = self.maxDepth(root.right)
+
+        # return max(left_height, right_height) + 1
+
+        # my solution
+        # left_height = 0
+        # right_height = 0
+        # # get left height
+        # if root.left is not None:
+        #     left_height = self.maxDepth(root.left)
+
+        # # get right height
+        # if root.right is not None:
+        #     right_height = self.maxDepth(root.right)
+
+        # return max(left_height, right_height) + 1
+
+        s = []
+
+        if root is not None:
+
+            s.append((1, root))
+
+        depth = 0
+
+        while len(s) > 0:
+
+            cur_depth, root = s.pop()
+
+            if root is not None:
+
+                depth = max(depth, cur_depth)
+
+                s.append((cur_depth + 1, root.left))
+                s.append((cur_depth + 1, root.right))
+
+        return depth
+
+
+b1 = BinaryTreeNode(20)
+b1.insert(10)
+b1.insert(30)
+b1.insert(31)
+b1.insert(32)
+
+print(b1.maxDepth(b1))
