911eb79ddd3e8b326f49f5571a875e7e599bb009
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-02 22:26:01 -0500

    paths

--- nc/s3/hw2.py
+++ nc/s3/hw2.py
@@ -47,3 +47,52 @@ def binaryTreeInOrderTraversal(root, inorder=None):
             binaryTreeInOrderTraversal(root.right, inorder)
 
         return inorder
+
+
+def treePaths(t):
+
+    if t is None:
+
+        return []
+
+    s = [[t]]
+    paths = list()
+
+    while len(s) > 0:
+
+        path = s.pop()
+        node = path[-1]
+
+        if node.left is None and node.right is None:
+
+            paths.append(path)
+
+        if node.left is not None:
+
+            new_p = list(path) + [node.left]
+            s.append(new_p)
+
+        if node.right is not None:
+
+            new_p = list(path) + [node.right]
+            s.append(new_p)
+
+    result = list()
+
+    for p in paths:
+
+        to_str = ""
+
+        for i in range(len(p)):
+
+            if i == len(p) - 1:
+
+                to_str += str(p[i].value)
+
+            else:
+
+                to_str += f"{str(p[i].value)}->"
+
+        result.append(to_str)
+
+    return result[::-1]
