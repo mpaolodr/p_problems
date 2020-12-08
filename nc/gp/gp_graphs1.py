a56574d39d7cf5d4c5fed486b0f685d5e85045f7
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-07 22:49:54 -0500

    pseudocode

--- nc/gp/gp_graphs1.py
+++ nc/gp/gp_graphs1.py
@@ -5,3 +5,15 @@ class GraphNode:
         self.label = label
         self.color = None
         self.neighbors = set()
+
+
+def color_graph(graph, colors):
+
+    # iterate over each node of the graph
+
+    # handle if an infinite loop is occuring
+
+    # get the node's neighbors colors
+    # check if a color is illegal
+
+    # assign the first legal color
