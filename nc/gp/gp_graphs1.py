a34cb3656062c595fae743b4b8b8f1b965cd65f7
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-07 23:46:08 -0500

    'numIslands

--- nc/gp/gp_graphs1.py
+++ nc/gp/gp_graphs1.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class GraphNode:
 
     def __init__(self, label):
@@ -79,3 +82,119 @@ color_graph(graph, colors)
 for n in nodes:
 
     print(n.color)
+
+
+def num_islands(grid):
+
+    # if there is no grid or the grid is empty then
+    if grid is None or len(grid) == 0:
+        # return zero
+        return 0
+
+    # store some lens and an island count
+
+    nr = len(grid)
+    nc = len(grid[0])
+
+    island_counts = 0
+
+    #  iterate over each row
+    for row in range(nr):
+
+        #  iterate over each col
+        for col in range(nc):
+
+            # check if our grid ar[row][col] is 1
+            if grid[row][col] == "1":
+
+                # increment the number of islands
+                island_counts += 1
+
+                # mark it as visited
+                grid[row][col] = "0"
+
+                # set up a queue of neighbors
+                neighbors = deque()
+
+                # append row * number_col + col to neighbors
+                neighbors.append(row * nc + col)
+
+                # while there are still neighbors
+                while len(neighbors) != 0:
+
+                    # set up an id based on a current neighbor
+                    id = neighbors.popleft()
+
+                    # set the row
+                    row = id // nc
+                    # set the column
+                    col = id % nc
+
+                    # check all the directions for connected components
+
+                    #  check left
+                    if row - 1 >= 0 and grid[row - 1][col] == "1":
+
+                        neighbors.append((row - 1) * nc + col)
+                        grid[row - 1][col] = "0"
+
+                    # check right
+                    if row + 1 < nr and grid[row + 1][col] == "1":
+
+                        neighbors.append((row + 1) * nc + col)
+                        grid[row + 1][col] = "0"
+
+                    # check top
+                    if col - 1 >= 0 and grid[row][col - 1] == "1":
+
+                        neighbors.append(row * nc + (col - 1))
+                        grid[row][col - 1] = "0"
+
+                    # check bot
+                    if col + 1 < nc and grid[row][col + 1] == "1":
+
+                        neighbors.append(row * nc + (col + 1))
+                        grid[row][col + 1] = "0"
+
+    return island_counts
+
+
+"""
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+
+"""
+
+grid1 = [
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]
+
+
+grid2 = [
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]
+
+
+print(num_islands(grid1))
+print(num_islands(grid2))
