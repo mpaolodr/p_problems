69d62af4174496eb489844508004b8f57a8b9ab2
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-07 23:01:15 -0500

    function done

--- nc/gp/gp_graphs1.py
+++ nc/gp/gp_graphs1.py
@@ -10,10 +10,42 @@ class GraphNode:
 def color_graph(graph, colors):
 
     # iterate over each node of the graph
+    for node in graph:
 
-    # handle if an infinite loop is occuring
+        # handle if an infinite loop is occuring
+        if node in node.neighbors:
+            raise Exception("impossible to find a legal solution")
 
-    # get the node's neighbors colors
-    # check if a color is illegal
+        # get the node's neighbors colors
+        # check if a color is illegal
+        illegal_colors = set([
+            neighbor.color for neighbor in node.neighbors if neighbor.color
+        ])
 
-    # assign the first legal color
+        # assign the first legal color
+        # node.color = colors - illegal_colors # color present in colors but not in illegal colors
+
+        for color in colors:
+
+            if color not in illegal_colors:
+
+                node.color = color
+                break
+
+    return graph
+
+
+g = {}
+
+nodes = [
+    GraphNode("A"), GraphNode("B"), GraphNode(
+        "C"), GraphNode("D"), GraphNode("E")
+]
+
+colors = ["black", "white", "red", "yellow", "blue"]
+
+for n in nodes:
+
+    g[n.label] = n
+
+print(g)
