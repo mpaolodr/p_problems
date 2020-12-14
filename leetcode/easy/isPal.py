5d9ff61ffa76a6aeb8d914aea773b4256c62e5ca
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-14 18:29:11 -0500

    optimal solution isPalindrome

--- leetcode/easy/isPal.py
+++ leetcode/easy/isPal.py
@@ -1,8 +1,25 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
 
-        if str(x) == str(x)[::-1]:
+def isPalindrome(x: int) -> bool:
 
-            return True
+    if x < 0 or (x % 10 == 0 and x != 0):
 
         return False
+
+    reverted_number = 0
+
+    while x > reverted_number:
+
+        reverted_number = reverted_number * 10 + x % 10
+
+        x = x // 10
+
+    return x == reverted_number or x == reverted_number // 10
+# def isPalindrome(self, x: int) -> bool:
+
+#     if str(x) == str(x)[::-1]:
+
+#         return True
+
+
+#     return False
+print(isPalindrome(12221))
