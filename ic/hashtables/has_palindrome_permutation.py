043bbe896c785f39b8c0ea914d4763d9772420e3
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-15 20:35:44 -0500

    different solution

--- ic/hashtables/has_palindrome_permutation.py
+++ ic/hashtables/has_palindrome_permutation.py
@@ -1,20 +1,45 @@
 # my solution
 
 
+# def has_palindrome_permutation(the_string):
+
+#     # Check if any permutation of the input is a palindrome
+
+#     check = set()
+
+#     for char in the_string:
+
+#         if char not in check:
+
+#             check.add(char)
+
+#     if len(check) == len(the_string) - (len(the_string) // 2):
+
+#         return True
+
+#     return False
+
+
+# different implementation
+
 def has_palindrome_permutation(the_string):
 
-    # Check if any permutation of the input is a palindrome
-
-    check = set()
+    count_dict = dict()
 
     for char in the_string:
 
-        if char not in check:
+        if char not in count_dict:
 
-            check.add(char)
+            count_dict[char] = 0
 
-    if len(check) == len(the_string) - (len(the_string) // 2):
+        count_dict[char] += 1
 
-        return True
+    odd = 0
 
-    return False
+    for k in count_dict:
+
+        if count_dict[k] % 2 != 0:
+
+            odd += 1
+
+    return odd <= 1
