45b177d149c89a420f590557e16a7a60aaa34a74
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-15 20:21:37 -0500

    optimal solution for can movie fill length

--- ic/hashtables/can_two_movies_fill_length.py
+++ ic/hashtables/can_two_movies_fill_length.py
@@ -1,34 +1,53 @@
 # my solution
 
 
+# def can_two_movies_fill_flight(movie_lengths, flight_length):
+
+#     # Determine if two movie runtimes add up to the flight length
+
+#     m_dict = dict()
+
+#     for i in range(len(movie_lengths)):
+
+#         if movie_lengths[i] not in m_dict:
+
+#             m_dict[movie_lengths[i]] = list()
+
+#         m_dict[movie_lengths[i]].append(i)
+
+#     for key in m_dict:
+
+#         target = max(flight_length, key) - min(flight_length, key)
+
+#         if target in m_dict:
+
+#             if target == key:
+
+#                 if len(m_dict[target]) > 1:
+
+#                     return True
+
+#             else:
+
+#                 return True
+
+#     return False
+
+# Optimal solution
 def can_two_movies_fill_flight(movie_lengths, flight_length):
 
     # Determine if two movie runtimes add up to the flight length
 
-    m_dict = dict()
+    seen = set()
 
-    for i in range(len(movie_lengths)):
+    for fm in movie_lengths:
 
-        if movie_lengths[i] not in m_dict:
+        sm = flight_length - fm
 
-            m_dict[movie_lengths[i]] = list()
+        if sm in seen:
 
-        m_dict[movie_lengths[i]].append(i)
+            return True
 
-    for key in m_dict:
-
-        target = max(flight_length, key) - min(flight_length, key)
-
-        if target in m_dict:
-
-            if target == key:
-
-                if len(m_dict[target]) > 1:
-
-                    return True
-
-            else:
-
-                return True
+        seen.add(fm)
 
     return False
