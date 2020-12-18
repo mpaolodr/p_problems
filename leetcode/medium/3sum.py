a7034454170c080693d71e515decdcdc6d874258
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-17 23:28:24 -0500

    2 pointer approach in 3sum

--- leetcode/medium/3sum.py
+++ leetcode/medium/3sum.py
@@ -1,61 +1,100 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
-        if len(nums) < 3:
+        # two pointer approach
 
-            return []
+        sorted_nums = sorted(nums)
+        results = list()
 
-        sum_dict = dict()
-        count_dict = dict()
+        seen = set()
 
-        for i in range(len(nums)):
+        for i in range(len(sorted_nums)):
 
-            if nums[i] not in count_dict:
+            lo = i + 1
+            hi = len(sorted_nums) - 1
 
-                count_dict[nums[i]] = 0
+            while lo < hi:
 
-            count_dict[nums[i]] += 1
+                sum_vals = sorted_nums[i] + sorted_nums[lo] + sorted_nums[hi]
 
-            for j in range(len(nums)):
+                if sum_vals < 0:
 
-                if ((nums[i], nums[j]) not in sum_dict) and (nums[j], nums[i]) not in sum_dict:
+                    lo += 1
 
-                    sum_dict[nums[i], nums[j]] = (nums[i] + nums[j]) * -1
+                elif sum_vals > 0:
 
-        tracker = set()
-        pos_results = list()
-
-        checker = set(nums)
-
-        for k in sum_dict:
-
-            if sum_dict[k] in checker and tuple(sorted([sum_dict[k], k[0], k[1]])) not in tracker:
-
-                if sum_dict[k] == k[0] and sum_dict[k] == k[1]:
-
-                    if count_dict[sum_dict[k]] > 2:
-
-                        pos_results.append([sum_dict[k], k[0], k[1]])
-
-                elif sum_dict[k] == k[0] or sum_dict[k] == k[1]:
-
-                    if count_dict[sum_dict[k]] > 1:
-
-                        pos_results.append([sum_dict[k], k[0], k[1]])
-
-                elif k[0] == k[1]:
-
-                    if count_dict[k[0]] >= 2:
-
-                        pos_results.append([sum_dict[k], k[0], k[1]])
+                    hi -= 1
 
                 else:
 
-                    pos_results.append([sum_dict[k], k[0], k[1]])
+                    if (sorted_nums[i], sorted_nums[lo], sorted_nums[hi]) not in seen:
 
-                tracker.add(tuple(sorted([sum_dict[k], k[0], k[1]])))
+                        seen.add(
+                            (sorted_nums[i], sorted_nums[lo], sorted_nums[hi]))
 
-        return pos_results
+                        results.append(
+                            [sorted_nums[i], sorted_nums[lo], sorted_nums[hi]])
+
+                    lo += 1
+                    hi -= 1
+
+        return results
+
+        # if len(nums) < 3:
+
+        #     return []
+
+        # sum_dict = dict()
+        # count_dict = dict()
+
+        # for i in range(len(nums)):
+
+        #     if nums[i] not in count_dict:
+
+        #         count_dict[nums[i]] = 0
+
+        #     count_dict[nums[i]] += 1
+
+        #     for j in range(len(nums)):
+
+        #         if ((nums[i], nums[j]) not in sum_dict) and (nums[j], nums[i]) not in sum_dict:
+
+        #             sum_dict[nums[i], nums[j]] = (nums[i] + nums[j]) * -1
+
+        # tracker = set()
+        # pos_results = list()
+
+        # checker = set(nums)
+
+        # for k in sum_dict:
+
+        #     if sum_dict[k] in checker and tuple(sorted([sum_dict[k], k[0], k[1]])) not in tracker:
+
+        #         if sum_dict[k] == k[0] and sum_dict[k] == k[1]:
+
+        #             if count_dict[sum_dict[k]] > 2:
+
+        #                 pos_results.append([sum_dict[k], k[0], k[1]])
+
+        #         elif sum_dict[k] == k[0] or sum_dict[k] == k[1]:
+
+        #             if count_dict[sum_dict[k]] > 1:
+
+        #                 pos_results.append([sum_dict[k], k[0], k[1]])
+
+        #         elif k[0] == k[1]:
+
+        #             if count_dict[k[0]] >= 2:
+
+        #                 pos_results.append([sum_dict[k], k[0], k[1]])
+
+        #         else:
+
+        #             pos_results.append([sum_dict[k], k[0], k[1]])
+
+        #         tracker.add(tuple(sorted([sum_dict[k], k[0], k[1]])))
+
+        # return pos_results
 
 
 """
@@ -157,7 +196,7 @@ class Solution:
 
                 sum_values = sorted_nums[i] + sorted_nums[lo] + sorted_nums[hi]
 
-                if abs(target - sum_values) < diff:
+                if abs(target - sum_values) < abs(diff):
 
                     diff = target - sum_values
 
