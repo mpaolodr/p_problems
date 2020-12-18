b478ed7adee6fa77c1de83be0a041a5e121df500
mpaolodr <mpaolodr_1504@yahoo.com>
2020-12-17 23:59:17 -0500

    demo2

--- nc/gp/gp_ht2.py
+++ nc/gp/gp_ht2.py
@@ -32,6 +32,9 @@ Notes:
 """
 
 
+import collections
+
+
 def top_k_frequent(words, k):
     """
     Input:
@@ -68,3 +71,71 @@ print(top_k_frequent(["lambda", "school", "rules", "lambda",
                       "school", "rocks"], 2))  # => ["lambda", "school"]
 print(top_k_frequent(["the", "sky", "is", "cloudy", "the", "the",
                       "the", "cloudy", "is", "is"], 4))  # => ["the", "is", "cloudy", "sky"]
+
+
+# demo 2
+"""
+Given a string, sort it in decreasing order based on the frequency of characters.
+Example 1:
+```plaintext
+Input:
+"free"
+Output:
+"eefr"
+Explanation:
+'e' appears twice while 'f' and 'r' appear once.
+So 'e' must appear before 'f' and 'r'. Therefore, "eerf" is also a valid answer.
+```
+Example 2:
+```plaintext
+Input:
+"dddbbb"
+Output:
+"dddbbb"
+Explanation:
+Both 'd' and 'b' appear three times, so "bbbddd" is also a valid answer.
+Note that "dbdbdb" is incorrect, as the same characters must be together.
+```
+Example 3:
+```plaintext
+Input:
+"Bbcc"
+Output:
+"ccBb"
+Explanation:
+"ccbB" is also a valid answer, but "Bbcc" is incorrect.
+Note that 'B' and 'b' are treated as two different characters.
+```
+"""
+
+# use the counter from collections to save us some time
+
+
+def frequency_sort(s: str) -> str:
+    """
+    Inputs:
+    s -> str
+    Output:
+    str
+    """
+    # Your code here
+    # count up all of the occurrences of each letter
+    # ref: https://docs.python.org/3/library/collections.html#collections.Counter
+    letters_count = collections.Counter(s)
+
+    # create a list to build the string
+    string_build = list()  # []
+
+    # iterate over the sorted counts (using the ".most_common()" method) extracting the letter and frequency key value pair
+    for letter, frequency in letters_count.most_common():
+        # letter * frequency
+        # "V" * 5 -> "VVVVV"
+        # append the letter * frequency to the string_build list
+        string_build.append(letter * frequency)
+
+    # turn the list back in to a string and return it (use a join?)
+    return "".join(string_build)
+
+
+print(frequency_sort("free"))  # => "eefr"
+print(frequency_sort("dddbbb"))  # => "dddbbb"
