ab7528e161a2d8076778b6dfd3c5120715185385
mpaolodr <mpaolodr_1504@yahoo.com>
2021-02-12 22:46:07 -0500

    better digits product

--- codesignal/digitsProduct.py
+++ codesignal/digitsProduct.py
@@ -1,21 +1,53 @@
-def digitsProduct(product):
+# def digitsProduct(product):
 
-    num = 1
+#     num = 1
 
-    while num <= 100000:
+#     while num <= 100000:
 
-        num_split = list(str(num))
+#         num_split = list(str(num))
 
-        running_total = 1
+#         running_total = 1
 
-        for n in num_split:
+#         for n in num_split:
 
-            running_total *= int(n)
+#             running_total *= int(n)
 
-        if running_total == product:
+#         if running_total == product:
 
-            return num
+#             return num
 
-        num += 1
+#         num += 1
 
-    return -1
+#     return -1
+
+
+def digitsProduct(p):
+
+    if p == 0:
+
+        return 10
+
+    elif p == 1:
+
+        return 1
+
+    result = list()
+
+    while p > 1:
+
+        for num in range(9, 1, -1):
+
+            if p % num == 0:
+
+                p /= num
+
+                result.append(num)
+                break
+        else:
+
+            return -1
+
+    return "".join([str(n) for n in sorted(result)])
+
+
+print(digitsProduct(450))
