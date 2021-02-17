09d1b7467cc799c89a1f5999cc2f7a2f78bca2b3
mpaolodr <mpaolodr_1504@yahoo.com>
2021-02-17 13:44:12 -0500

    auto expanding text area

--- react-components/components/src/App.js
+++ react-components/components/src/App.js
@@ -1,63 +1,57 @@
 import React, { useState } from 'react';
 
+import styled from 'styled-components';
+
 import Datepicker from './components/Datepicker';
 import BtnLoader from './components/btn-loader/BtnLoader';
+
+const TextContainer = styled.div`
+  border: 2px solid #cad1cf;
+  width: 30%;
+  border-radius: 21.5px;
+  overflow: hidden;
+  padding: 0;
+  display: flex;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: auto;
+  padding: 0.4rem 0.8rem;
+  background: #f1f1f1;
+  border: none;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 function App() {
   const [date, setDate] = useState('02-10-2021');
   const [loading, setLoading] = useState(false);
+  const [sHeight, setSHeight] = useState(null);
 
-  const toggleLoading = () => {
-    setLoading(true);
+  console.log('SHEIGHT: ', sHeight);
 
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+  const resize = (e) => {
+    if (!sHeight) {
+      setSHeight(e.target.scrollHeight);
+    } else {
+      if (sHeight !== e.target.scrollHeight) {
+        e.target.attributes.rows.nodeValue =
+          parseInt(e.target.attributes.rows.nodeValue) + 1;
+        setSHeight(e.target.scrollHeight);
+        console.log(e);
+      }
+    }
   };
 
   return (
     <div className='App'>
-      <div
-        className='testContainer'
-        style={{
-          outline: '2px solid blue',
-          height: '20vh',
-          width: '50%',
-          margin: '0 auto',
-          display: 'flex',
-          alignItems: 'center',
-          flexWrap: 'wrap',
-        }}
-      >
-        <div
-          className='box1'
-          style={{
-            flex: '0 0 20%',
-            marginRight: 'auto',
-            outline: '2px solid black',
-          }}
-        >
-          box 1
-        </div>
-
-        <div
-          className='box2'
-          style={{
-            display: 'none',
-            flex: '0 0 75%',
-            outline: '2px solid tomato',
-          }}
-        >
-          box2
-        </div>
-
-        <div
-          className='box3'
-          style={{ flex: '0 0 75%', outline: '2px solid green' }}
-        >
-          box3
-        </div>
-      </div>
+      <TextContainer className='textarea-container'>
+        <TextArea rows='1' onChange={resize}></TextArea>
+      </TextContainer>
     </div>
   );
 }
